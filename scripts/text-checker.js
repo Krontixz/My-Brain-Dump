bffd635f5ce72b018ba7a7fd67e33437da425118
Krontixz <zaki.coding0119@gmail.com>
2026-08-31 16:22:30 -0700

    Change console log to alert for text inclusion

--- scripts/text-checker.js
+++ scripts/text-checker.js
@@ -2,5 +2,5 @@ function textChecker() {
   let typedText = document.getElementById("typedTextBox");
   let typedTextReturn = typedText.value;
   let typedTextIncludes = typedTextReturn.includes("coding") || typedTextReturn.includes("zaki");
-  console.log(`Your text includes coding or zaki: ${typedTextIncludes}`);
+  alert(`Your text includes coding or zaki: ${typedTextIncludes}`);
 }
