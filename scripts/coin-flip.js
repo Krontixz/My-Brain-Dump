216c3f0c9eb9ca99d1153e0925986f51873fbc7c
Krontixz <zaki.coding0119@gmail.com>
2026-09-01 16:07:18 -0700

    Create coin-flip.js

---
+++ scripts/coin-flip.js
@@ -0,0 +1,11 @@
+function coinFlip() {
+  let guess = document.getElementById("guessBox").value;
+  if (guess === "Heads" || guess === "heads" || guess === "tails" || guess === "Tails") {
+    let result = Math.random() < 0.5 ? "heads" : "tails";
+    alert(`It landed on ${result}`);
+  }
+  else {
+    alert(`That is not a valid answer`);
+  }
+}
+    
