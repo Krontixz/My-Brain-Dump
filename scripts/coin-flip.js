a209c1c0091ae0dbd125eaa15c296fbc7fbb421e
Krontixz <zaki.coding0119@gmail.com>
2026-09-01 16:20:15 -0700

    Fix typo in coinFlip function for guess retrieval

--- scripts/coin-flip.js
+++ scripts/coin-flip.js
@@ -1,5 +1,5 @@
 function coinFlip() {
-  let guess = document.getElementById("guess-box").value.toLowerCase;
+  let guess = document.getElementById("guess-box").value.toLowerCase();
   if (guess === "heads" || guess === "tails") {
     let result = Math.random() < 0.5 ? "heads" : "tails";
     alert(`It landed on ${result}`);
