a1af78e0d374f548be29c482acd11eb502c20881
Krontixz <zaki.coding0119@gmail.com>
2026-09-01 16:15:28 -0700

    Update coin-flip.js

--- scripts/coin-flip.js
+++ scripts/coin-flip.js
@@ -1,6 +1,6 @@
 function coinFlip() {
   let guess = document.getElementById("guess-box").value.toLowerCase;
-  if (guess === "eads" || guess === "tails") {
+  if (guess === "heads" || guess === "tails") {
     let result = Math.random() < 0.5 ? "heads" : "tails";
     alert(`It landed on ${result}`);
   }
