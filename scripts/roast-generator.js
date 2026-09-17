c5fe980186a311f8ce2b8e5b08a097469e718d2c
Krontixz <zaki.coding0119@gmail.com>
2026-09-17 10:56:30 -0700

    Create roast-generator.js

---
+++ scripts/roast-generator.js
@@ -0,0 +1,23 @@
+function roastGenerator() {
+  const adjectives = [
+    "stupid",
+    "ugly",
+    "stinky",
+    "dumb",
+    "poopy",
+    "disgusting",
+    "dispicable",
+    "ai slop",
+    "life is a mistake",
+    "death luma",
+    "built like king kong",
+    "big eared elf",
+    "teachers pet"
+    ];
+  let randomPosition = Math.floor(Math.random() * adjective.length);
+  let pickedAdjective = adjectives[randomPosition];
+  const nouns = [
+    "potato",
+    "godzilla",
+    "brat",
+    ]
